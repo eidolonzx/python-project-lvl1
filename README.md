a09baae7ff8d5062f9198ffa73f10c783248ac78
Michael Solovyev <eidolonzx@gmail.com>
2022-04-05 06:46:23 -0700

    added brain-prime game

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 ![linter-check](https://github.com/eidolonzx/python-project-lvl1/actions/workflows/linter-check.yml/badge.svg)
 
-ASCIINEMA
+### ASCIINEMA
 
 Step 5 - https://asciinema.org/a/JzAsSFEK4zzlnJtrCfo4UcHkk
 
@@ -14,3 +14,5 @@ Step 6 - https://asciinema.org/a/MVOXH5vkEkthIe0m9aFMthCpv
 Step 7 - https://asciinema.org/a/P7Ag8gIJcpibWRSGzwNiooI6c
 
 Step 8 - https://asciinema.org/a/QkSDQjN06IgsIIiEMHGBK9tG1
+
+Step 9 - https://asciinema.org/a/YDOlKRDwfZMpPOriRxvQXVG1b
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from brain_games.gameloop import make_game_loop
+import random
+
+
+game_name = "Brain Prime"
+game_rule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(number):
+    def iterator(counter):
+        if number == counter:
+            return True
+        if number % counter == 0:
+            return False
+        return iterator(counter + 1)
+    return iterator(2)
+
+
+def game_condition():
+    number = random.randint(1, 99)
+    question = f"Is this number prime? {number}"
+    right_answer = "yes" if is_prime(number) else "no"
+    condition = [question, right_answer]
+    return condition
+
+
+def main():
+    make_game_loop(game_name, game_rule, game_condition)
+
+
+if __name__ == '__main__':
+    main()
